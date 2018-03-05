47468d37573ea8db8b84c3f553c75da65e11a6d9
Brahim Bahaida <bbahieda@gmail.com>
2018-03-05 16:14:48 +0100

    Added spring project basic stratcture and dependencies

---
+++ src/main/java/com/bahaida/zooz/ZoozApplication.java
@@ -0,0 +1,12 @@
+package com.bahaida.zooz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoozApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bahaida/zooz/ZoozApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bahaida.zooz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZoozApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
